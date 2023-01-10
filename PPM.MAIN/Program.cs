0beff7bd1f7ba5a4a5ac1c45a14920386b6b08ed
Shoba Mandlole <SMandlole@Prolifics.com>
2023-01-10 17:14:25 +0530

    First Commit

---
+++ PPM.MAIN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PPM.MAIN.PPM.UI;
+
+ class Program
+ {
+    public static void Main(string[] args)
+    {
+        viewing view = new viewing();
+        view.view();
+    
+    }
+}
